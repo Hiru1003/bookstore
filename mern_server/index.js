8040f3179ee7c259c9f3abc142abdac5cabeafde
Hiru1003 <hirumiperera10@gmail.com>
2024-05-18 08:32:15 +0530

    feat: db connection

--- mern_server/index.js
+++ mern_server/index.js
@@ -11,6 +11,36 @@ app.get('/', (req, res) => {
   res.send('Hello World! ')
 })
 
+// mongodb confiq here
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mern-bookstore:zrv0tdYJbu9V8Hu3@cluster0.hcvzaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
