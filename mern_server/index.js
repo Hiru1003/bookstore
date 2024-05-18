d30fb907deffe5b4887f298a254c0d74470bdfd9
Hiru1003 <hirumiperera10@gmail.com>
2024-05-18 08:37:39 +0530

    feat: add collection

--- mern_server/index.js
+++ mern_server/index.js
@@ -26,6 +26,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const bookCollections = client.db("BookInventory").collection("Books");
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
