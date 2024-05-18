00287c72bacaba0673333e6500fb128edd4a474f
Hiru1003 <hirumiperera10@gmail.com>
2024-05-18 08:12:09 +0530

    feat: middlewear

--- mern_server/index.js
+++ mern_server/index.js
@@ -3,6 +3,10 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+// middlewear 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World! ')
 })
