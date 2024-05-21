c3cf557d0e5a20c57a63709077ebb55f1247200c
Hiru1003 <hirumiperera10@gmail.com>
2024-05-21 13:58:19 +0530

    feat: delete a item from db

--- mern_server/index.js
+++ mern_server/index.js
@@ -73,6 +73,18 @@ async function run() {
         res.send(result);
     })
 
+
+    // delete a item from db
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(filter);
+      res.send(result);
+  })
+
+
+  
+
   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
