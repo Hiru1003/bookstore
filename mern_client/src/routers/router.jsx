f262f4b5109a4ef898ef06379593479f78cdaec9
Hiru1003 <hirumiperera10@gmail.com>
2024-05-24 10:33:36 +0530

    feat: call app

--- mern_client/src/routers/router.jsx
+++ mern_client/src/routers/router.jsx
@@ -2,11 +2,12 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import App from "../App";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <App/>,
     },
   ]);
 
