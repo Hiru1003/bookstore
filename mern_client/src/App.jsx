8b71da4eabf5314ad05b659c4c14ba9b0d04c7a2
Hiru1003 <hirumiperera10@gmail.com>
2024-05-25 09:13:51 +0530

    feat: logo

--- mern_client/src/App.jsx
+++ mern_client/src/App.jsx
@@ -6,40 +6,6 @@ import Navbar from './components/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
-const Navbar =() => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const  [isStickly, setIsStickly] = useState(false);
-
-  //toggle menu
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  }
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if(window.scrollY > 100){
-        setIsStickly(true);
-      }
-      else{
-        setIsStickly(false);
-      }
-    }
-    window.addEventListener("scroll", handleScroll);
-
-    return() => {
-      window.addEventListener("scroll", handleScroll);
-    }
-  }, [])
-
-  const navItems =[
-    {link: "Home", path: "/"},
-    {link: "About", path: "/about"},
-    {link: "Shop", path: "/shop"},
-    {link: "Sell your book", path: "/admin/dashboard"},
-    {link: "Blog", path: "/blog"}
-  ]
-}
-
   return (
     <>
       <Navbar/>
--- mern_client/src/components/Navbar.jsx
+++ mern_client/src/components/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { TbCircleLetterBFilled } from "react-icons/tb";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  // Toggle menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+  const navItems = [
+    { link: "Home", path: "/" },
+    { link: "About", path: "/about" },
+    { link: "Shop", path: "/shop" },
+    { link: "Sell your book", path: "/admin/dashboard" },
+    { link: "Blog", path: "/blog" }
+  ];
+
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <header className={isSticky ? "sticky" : ""}>
+      <nav>
+        <div>
+          {/* logo */}
+          <Link to="/" className='text-2xl font-bold flex items-center gap-3'>
+            <TbCircleLetterBFilled className='inline-block' style={{ fontSize: '2rem' }}/>Bookly
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
