cd5fccace4232226b7dbfcbff5b0afb6bf7b999a
Hiru1003 <hirumiperera10@gmail.com>
2024-05-25 08:04:47 +0530

    feat: toggleMenu

--- mern_client/src/App.jsx
+++ mern_client/src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
+
+const Navbar =() => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const  [isStickly, setIsStickly] = useState(false);
+
+  //toggle menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 100){
+        setIsStickly(true);
+      }
+      else{
+        setIsStickly(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return() => {
+      window.addEventListener("scroll", handleScroll);
+    }
+  })
+}
 
   return (
     <>
