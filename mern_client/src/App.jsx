2eccb71c9f3642530b7d2fa9875103b148a5efba
Hiru1003 <hirumiperera10@gmail.com>
2024-05-25 06:58:13 +0530

    feat: navbar importing

--- mern_client/src/App.jsx
+++ mern_client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <nav>Site logo</nav>
+      <Navbar/>
       <Outlet/>
     </>
   )
