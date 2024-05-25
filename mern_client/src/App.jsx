d1c74a06fd94611c294fa21f5bfb5be4a9f0c66e
Hiru1003 <hirumiperera10@gmail.com>
2024-05-25 06:47:20 +0530

    feat: nav testing

--- mern_client/src/App.jsx
+++ mern_client/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Hello mern!</div>
+    <nav>Site logo</nav>
+      <Outlet/>
     </>
   )
 }
