aaba87c8fdeff50f458d414d53b79810e2ed0e1f
Hiru1003 <hirumiperera10@gmail.com>
2024-05-26 07:41:33 +0530

    feat: swiper with images

--- mern_client/src/home/Bannercard.jsx
+++ mern_client/src/home/Bannercard.jsx
@@ -13,22 +13,20 @@ import { EffectCards } from 'swiper/modules';
 
 const Bannercard = () => {
   return (
-    <div>
+    <div className='banner'>
       <Swiper
         effect={'cards'}
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
       </Swiper>
     </div>
   )
