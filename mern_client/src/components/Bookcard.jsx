b5016975a11e384a4fcc2403e970c542f8b3e090
Hiru1003 <hirumiperera10@gmail.com>
2024-05-26 12:46:25 +0530

    feat: swiper

--- mern_client/src/components/Bookcard.jsx
+++ mern_client/src/components/Bookcard.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+//import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const Bookcard = ({headline, books}) => {
   return (
     <div className='my-16 px-4 lg:px24'>
       <h2 className='text-5xl text-center font-bold text-black my-5'>{headline}</h2>
+
+      <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </div>
     </div>
   )
 }
