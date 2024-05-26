322a1398feb4df29cfc68a3ad5a940eb4fb258c2
Hiru1003 <hirumiperera10@gmail.com>
2024-05-26 07:23:11 +0530

    feat: bannercard.css

--- mern_client/src/home/Bannercard.jsx
+++ mern_client/src/home/Bannercard.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 
-//import './styles.css';
+import './Bannercard.css';
 
 // import required modules
 import { EffectCards } from 'swiper/modules';
