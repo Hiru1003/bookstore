71d6fa55e66d9c00989f90b006620cebed7c9d2a
Hiru1003 <hirumiperera10@gmail.com>
2024-05-27 09:48:49 +0530

    feat: import img

--- mern_client/src/home/Favbooks.jsx
+++ mern_client/src/home/Favbooks.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import favimg from "../assets/favimg.png"
 
 const Favbooks = () => {
   return (
-    <div>
-      
+    <div className='px-4 lg:px-24 my-20'>
+      <div className='md:w-1/2'>
+        <img src={favimg}/>
+      </div>
     </div>
   )
 }
