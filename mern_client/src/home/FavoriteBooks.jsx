0a2c49bc894872d5065c4ed985fd85086c280b6b
Hiru1003 <hirumiperera10@gmail.com>
2024-05-27 07:07:46 +0530

    feat: fav book done

--- mern_client/src/home/FavoriteBooks.jsx
+++ mern_client/src/home/FavoriteBooks.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import Bookcard from '../components/Bookcard';
 
 const FavoriteBooks = () => {
-  const[books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => console.log(date))
-  }, [])
+    fetch("http://localhost:5000/all-books")
+      .then(res => res.json())
+      .then(data => setBooks(data)) // Corrected the typo
+      .catch(err => console.error("Error fetching books:", err));
+  }, []);
 
   return (
     <div>
-      <Bookcard books={books} headline="Best Seller Books"/>
+      <Bookcard books={books} headline="Best Seller Books" />
     </div>
-  )
-}
+  );
+};
 
-export default FavoriteBooks
+export default FavoriteBooks;
