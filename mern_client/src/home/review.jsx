67178bf689584e868851a7663f15b2cd43cc539a
Hiru1003 <hirumiperera10@gmail.com>
2024-05-27 13:02:15 +0530

    feat: review page swiper

--- mern_client/src/home/review.jsx
+++ mern_client/src/home/review.jsx
@@ -1,12 +1,78 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const review = () => {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import {FaStar} from "react-icons/fa6"
+
+//import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+const ReviewPage = () => {
   return (
     <div>
-      review
+      <div className='my-12 px-4 lg:px-24'>
+        <h2 className='text-5xl font-bold text-center mb-10 leading-snug'>Our Customers</h2>  
+
+        <div>
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <div>
+                <div className='text-amber-500 flex gap-2'>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                    <FaStar/>
+                </div>
+
+                <div>
+                    <p className='text-lg mt-5'>
+                        I  absolutely love shopping here! The selection of books is fantastic, and I've discovered so many new favorites thanks to their recommendations. The delivery is always fast and the customer service is top-notch. Highly recommend this site to all book lovers!
+                    </p>
+                </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+        </div>     
+      </div>
 
     </div>
   )
 }
 
-export default review
+export default ReviewPage
