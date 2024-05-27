bec381da65df47b27efb8bedc69b62e5def89751
Hiru1003 <hirumiperera10@gmail.com>
2024-05-27 09:03:38 +0530

    feat: bestseller books

---
+++ mern_client/src/home/BestSellerBooks.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import Bookcard from '../components/Bookcard';
+
+const BestSellerBooks = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/all-books")
+      .then(res => res.json())
+      .then(data => setBooks(data)) // Corrected the typo
+      .catch(err => console.error("Error fetching books:", err));
+  }, []);
+
+  return (
+    <div>
+      <Bookcard books={books} headline="Best Seller Books" />
+    </div>
+  );
+};
+
+export default BestSellerBooks;
