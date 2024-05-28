33510f9ca0a5e2bf5057354a97eb1ce4577cd193
Hiru1003 <hirumiperera10@gmail.com>
2024-05-28 10:57:02 +0530

    feat: form upto categories

--- mern_client/src/dashboard/SideBar.jsx
+++ mern_client/src/dashboard/SideBar.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
 import { Sidebar } from "flowbite-react";
 import { BiBuoy } from "react-icons/bi";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiArrowSmRight, HiBookOpen, HiChartPie, HiCloudUpload, HiInbox, HiLogin, HiLogout, HiShoppingBag, HiTable, HiUpload, HiUser, HiUserAdd, HiViewBoards } from "react-icons/hi";
 
 
 const SideBar = () => {
   return (
     <Sidebar aria-label="Sidebar with content separator example">
-      <Sidebar.Logo href="#" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdw3zU3LBFcBoYobQQC-4QQzQZnZURJp3YMakmeoblVd28JkFz2y7zgo1IHC7IhZCT9wk&usqp=CAU" className='w-14 h-14 rounded-md'/>
+      <Sidebar.Logo href="#" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdw3zU3LBFcBoYobQQC-4QQzQZnZURJp3YMakmeoblVd28JkFz2y7zgo1IHC7IhZCT9wk&usqp=CAU" className='w-14 h-14 rounded-md'>Admin User
+      </Sidebar.Logo>
       <Sidebar.Items>
         <Sidebar.ItemGroup>
           <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
             Dashboard
           </Sidebar.Item>
-          <Sidebar.Item href="/admin/dashboard/upload" icon={HiViewBoards}>
+          <Sidebar.Item href="/admin/dashboard/upload" icon={HiCloudUpload}>
             Upload Book
           </Sidebar.Item>
-          <Sidebar.Item href="/admin/dashboard/manage" icon={HiInbox}>
+          <Sidebar.Item href="/admin/dashboard/manage" icon={HiBookOpen}>
             Manage Book
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiUser}>
             Users
           </Sidebar.Item>
-          <Sidebar.Item href="/login" icon={HiShoppingBag}>
+          <Sidebar.Item href="/login" icon={HiLogin}>
             Sign In
           </Sidebar.Item>
-          <Sidebar.Item href="/register" icon={HiArrowSmRight}>
+          <Sidebar.Item href="/register" icon={HiUserAdd}>
             Sign Up
           </Sidebar.Item>
-          <Sidebar.Item href="/logout" icon={HiTable}>
+          <Sidebar.Item href="/logout" icon={HiLogout}>
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
--- mern_client/src/dashboard/Uploadbook.jsx
+++ mern_client/src/dashboard/Uploadbook.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Checkbox, Label, Select, TextInput } from "flowbite-react";
 
 const Uploadbook = () => {
+  const BookCategories = [
+    "Fiction",
+    "Non-Fiction",
+    "Science Fiction",
+    "Fantasy",
+    "Mystery",
+    "Thriller",
+    "Romance",
+    "Historical",
+    "Biography",
+    "Self-Help",
+    "Children's",
+    "Young Adult",
+    "Graphic Novels",
+    "Comics",
+    "Poetry",
+    "Drama",
+    "Classics",
+    "Adventure",
+    "Horror",
+    "Cooking",
+    "Art",
+    "Travel",
+    "Science",
+    "Technology",
+    "Health",
+    "History",
+    "Philosophy",
+    "Religion",
+    "Education",
+    "Business",
+    "Law",
+    "Music",
+    "Sports",
+    "Politics",
+    "Psychology",
+    "Spirituality",
+    "True Crime",
+    "Parenting",
+    "Humor",
+  ]
+  const [selectedBookCategories, setSelectedCategorie] = useState(BookCategories[0]);
+  const handleChangeSelectedValue =(event)=> {
+    //console.log(event.target.value);
+    selectedBookCategories(event.target.value);
+  }
+
   return (
-    <div>
-      UploadbOOK
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Upload Book</h2>
+
+      <form className="lg:w-[1180px] flex flex-col gap-4">
+        {/* First Row */}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+    
+          {/* title */}
+          <div>
+            <div className="mb-3 block">
+              <Label htmlFor="title" value="Book Title" />
+            </div>
+            <TextInput id="title" type="text" name='title' placeholder='Book Name' required />
+          </div>
+
+          {/* author */}  
+          <div>
+            <div className="mb-3 block">
+              <Label htmlFor="authorName" value="Author Name" />
+            </div>
+            <TextInput id="authorName" type="text" name='authorName' placeholder='Author Name' required />
+          </div>
+        </div>
+
+
+        {/* Second Row */}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+    
+          {/* imageUrl */}
+          <div>
+            <div className="mb-3 block">
+              <Label htmlFor="imageUrl" value="Book Image Url" />
+            </div>
+            <TextInput id="imageUrl" type="text" name='imageUrl' placeholder='Book Image Url' required />
+          </div>
+
+          {/* Category */}  
+          <div>
+            <div className="mb-3 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+            <select id='inputState' name='categoryName' className='w-full rounded' value={selectedBookCategories} onChange={handleChangeSelectedValue}>
+              {BookCategories.map(option => (
+                <option key={option} value={option}>{option}</option>
+              ))}
+            </select>
+
+          </div>
+        </div>
+
+
+      </form>
     </div>
-  )
+  );
 }
 
-export default Uploadbook
+export default Uploadbook;
