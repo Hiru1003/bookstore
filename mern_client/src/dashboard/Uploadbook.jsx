32459546cfab389dc3e999fb442931435cdc4ce5
Hiru1003 <hirumiperera10@gmail.com>
2024-05-28 15:40:04 +0530

    feat:handle book submission

--- mern_client/src/dashboard/Uploadbook.jsx
+++ mern_client/src/dashboard/Uploadbook.jsx
@@ -43,17 +43,37 @@ const Uploadbook = () => {
     "Parenting",
     "Humor",
   ]
+
   const [selectedBookCategories, setSelectedCategorie] = useState(BookCategories[0]);
   const handleChangeSelectedValue =(event)=> {
     console.log(event.target.value);
     selectedBookCategories(event.target.value);
   }
 
+  //handle book submission
+  const handleBookSubmit  = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const title = form.title.value;
+    const authorName = form.authorName.value;
+    const imageUrl = form.imageUrl.value;
+    const category = form.category.value;
+    const description = form.description.value;
+    const pdfUrl = form.pdfUrl.value;
+
+    const bookObj = {
+      title,authorName,imageUrl,category,description,pdfUrl
+    }
+
+    console.log(bookObj);
+  }
+
   return (
     <div className='px-4 my-12'>
       <h2 className='mb-8 text-3xl font-bold'>Upload Book</h2>
 
-      <form className="lg:w-[1180px] flex flex-col gap-4">
+      <form className="lg:w-[1180px] flex flex-col gap-4" >
         {/* First Row */}
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
     
@@ -91,7 +111,7 @@ const Uploadbook = () => {
             <div className="mb-3 block">
               <Label htmlFor="inputState" value="Book Category" className='text-xl'/>
             </div>
-            <select id='inputState' name='categoryName' className='w-full rounded ' value={selectedBookCategories} onChange={handleChangeSelectedValue}>
+            <select id='inputState' name='category' className='w-full rounded ' value={selectedBookCategories} onChange={handleChangeSelectedValue}>
               {BookCategories.map(option => (
                 <option key={option} value={option}>{option}</option>
               ))}
