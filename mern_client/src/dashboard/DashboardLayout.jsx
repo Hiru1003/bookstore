287b5de5dd15385307b6b3642206eb1361f33623
Hiru1003 <hirumiperera10@gmail.com>
2024-05-28 10:25:13 +0530

    feat: add components

--- mern_client/src/dashboard/DashboardLayout.jsx
+++ mern_client/src/dashboard/DashboardLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import SideBar from './SideBar'
 
 const DashboardLayout = () => {
   return (
-    <div>
-      
+    <div className='flex gap-4 flex-col md:flex-row'>
+      <SideBar/>
+      <Outlet/>
     </div>
   )
 }
