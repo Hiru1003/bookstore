162a1798a8319ac86031a1845aa8e04ccd542118
Hiru1003 <hirumiperera10@gmail.com>
2024-05-29 16:49:22 +0530

    feat: manage boook with table

--- mern_client/src/dashboard/ManageBooks.jsx
+++ mern_client/src/dashboard/ManageBooks.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Table } from "flowbite-react";`q`
+
+import { Button, Table } from "flowbite-react";`q`
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 const ManageBooks = () => {
 
   const[allBooks, setAllBooks] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/upload-book")
+    fetch("http://localhost:5000/all-books")
     .then(res => res.json())
     .then(data => setAllBooks(data))
   }, [])
@@ -18,78 +19,39 @@ const ManageBooks = () => {
 
       <Table striped className='lg:w-[1180px]'>
         <Table.Head>
-        <Table.HeadCell>Title</Table.HeadCell>
-          <Table.HeadCell>Product name</Table.HeadCell>
-          <Table.HeadCell>Color</Table.HeadCell>
+        <Table.HeadCell>No</Table.HeadCell>
+          <Table.HeadCell>Book name</Table.HeadCell>
+          <Table.HeadCell>Author Name</Table.HeadCell>
           <Table.HeadCell>Category</Table.HeadCell>
-          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>Prices</Table.HeadCell>
           <Table.HeadCell>
-            <span className="sr-only">Edit</span>
+            <span>Edit or Manage</span>
           </Table.HeadCell>
         </Table.Head>
-        <Table.Body className="divide-y">
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
+        {
+        allBooks.map((book, index) => 
+          <Table.Body className="divide-y" key={book._id}>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+           <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {index+1}
             </Table.Cell>
-            <Table.Cell>Sliver</Table.Cell>
-            <Table.Cell>Laptop</Table.Cell>
-            <Table.Cell>$2999</Table.Cell>
+            <Table.Cell>{book.title}</Table.Cell>
+            <Table.Cell>{book.authorName}</Table.Cell>
+            <Table.Cell>{book.category}</Table.Cell>
+            <Table.Cell>2500 LKR</Table.Cell>
             <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
+              <div className="flex items-center">
+                <Link to={`/admin/dashboard/edit-books/${book._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5">
+                  Edit
+                </Link>
+                <Button onClick={() => handleDelete(book._id)} className="bg-red-600 px-4 py-1 font-semibold text-white rounded hover:bg-black">Delete</Button>
+              </div>
             </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Microsoft Surface Pro
-            </Table.Cell>
-            <Table.Cell>White</Table.Cell>
-            <Table.Cell>Laptop PC</Table.Cell>
-            <Table.Cell>$1999</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
-            <Table.Cell>Black</Table.Cell>
-            <Table.Cell>Accessories</Table.Cell>
-            <Table.Cell>$99</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Google Pixel Phone
-            </Table.Cell>
-            <Table.Cell>Gray</Table.Cell>
-            <Table.Cell>Phone</Table.Cell>
-            <Table.Cell>$799</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Apple Watch 5</Table.Cell>
-            <Table.Cell>Red</Table.Cell>
-            <Table.Cell>Wearables</Table.Cell>
-            <Table.Cell>$999</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Edit
-              </a>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
+          </Table.Row> 
+          </Table.Body>
+         )
+        }
+
       </Table>
     </div>
   )
